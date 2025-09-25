426c4b07210897590d9f62db9783a123d04e10aa
Utsav Deep <utsavdeep01@gmail.com>
2025-09-25 18:13:21 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Bill Spliter with Google Gemini
+
+This is a simple application to split bills between multiple people. Where few items could be assigned to specific people and other items can be marked as common.
+The app can then calculate the split and show how much each person owes.
+
+## ToDo's
+- [ ] Add the screen to add persons to split between
+- [ ] Add the screen to upload receipt image(s)
+- [ ] Add the screen to that shows each item as a list
+- [ ] Add the calculation logic
+- [ ] Make it self-hostable
---
+++ src/APIKey.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+
+export default function APIKey() {
+
+    const [key, setKey] = useState('');
+
+    const handleSubmit = () => {
+        console.log("Submiting Key....")
+        console.log(key)
+    }
+
+    return (
+        <>
+            <div className="bg-gray-50 p-6 rounded-lg space-y-4">
+                <h2 className="text-xl font-semibold text-charcoal">Enter Your API Key</h2>
+                <p className="text-silver text-sm"  >You need a Google AI API Key to proceed. Get one from
+                    <a href="https://aistudio.google.com/" target="_blank" className="text-blue-500 hover:underline"> Google AI Studio</a>.
+                </p>
+                <div className="flex flex-col sm:flex-row gap-2">
+                    <input value={key} onChange={(e) => {
+                        setKey(e.target.value)
+                    }} className="flex-grow bg-white text-silver placeholder-silver rounded-md px-4 py-2 border border-charcoal focus:outline-none focus:ring-2 focus:ring-yellow-green" type="password" placeholder="Enter Google AI API Key"></input>
+                    <button className="bg-yellow-green text-charcoal font-bold px-6 py-2 rounded-md hover:bg-opacity-90 hover:transition-colors" onClick={handleSubmit} type="submit">Submit</button>
+                </div>
+            </div>
+        </>
+    )
+}
